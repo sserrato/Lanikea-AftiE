a7e921f6b447d0f3b7973fd3f5e90bee8ea6d832
Sergio Serrato <serrato@cal.berkeley.edu>
2015-08-20 15:00:45 -0700

    added date

--- models/tweet.js
+++ models/tweet.js
@@ -1,9 +1,10 @@
 var mongoose  = require('mongoose');
-// this is the tweetstream schema. we intend to embed the followersCount and friendsCount
+// this is the tweetstream schema. we intend to embed the followersCount and
 var tweetstreamSchema = new mongoose.Schema({
   searchTerm: {type: String},
   followersCount: {type: Number},
-  friendsCount: {type: Number}
+  friendsCount: {type: Number},
+  dateCreated: {type: Date, default: Date.now}
 })
 
 
