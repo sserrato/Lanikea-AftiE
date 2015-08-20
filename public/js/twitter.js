3c84ff52935d53b593bf171620547c2cf8626853
Sergio Serrato <serrato@cal.berkeley.edu>
2015-08-19 21:38:37 -0700

    twitter javascript

--- public/js/twitter.js
+++ public/js/twitter.js
@@ -1,46 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>GtagTweets</title>
-  <style type="text/css">
-  html, body, #map {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-  .stations, .stations svg {
-    position: absolute;
-  }
-  .stations svg {
-    width: 60px;
-    height: 20px;
-    padding-right: 100px;
-    font: 10px sans-serif;
-  }
-  .stations circle {
-    fill: rgba(255, 255, 255, 0.8);
-    -webkit-filter: blur(3px);
-    /*stroke: blue;
-    stroke-width: 1.5px;*/
-  }
-  </style>
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.js"></script>
-  <script type="text/javascript" src="/socket.io/socket.io.js"></script>
-  <script type="text/javascript" src="/js/twitter.js"></script>
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-  <script src="/js/stylized_markers.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
-</head>
-<body onload="initialize()">
-  <h1>Twitter Stream</h1>
-  <form action="/markers.html" name="search">
-  <div class="form-group">
-    <input type="text" class="form-control">
-  </div>
-  <input type="submit" class="btn btn-default">
-  </form>
-  <div id="chart"></div>
-  <div id="map" style="height:100%;width:100%"></div>
-</body>
-</html>
+$(function (){
+  var socket = io();
+  socket.on('connect', function() {
+    console.log('Connected!');
+  });
+  socket.on('tweets', function(tweet) {
+    console.log(tweet);
+    var html = '<div class="row"><div class="col-md-6 col-md-offset-3 tweet"><img src="' + tweet.pic + '" class="avatar pull-left"/><div class="names"><span class="full-name">' + tweet.screen_name + ' </span><span class="username">@' +tweet.screen_name + '</span></div><div class="contents"><span class="text">' + tweet.text + '</span></div></div></div>';
+    $('#tweet-container').prepend(html);
+  });
+  $('form').submit(function(e){
+    e.preventDefault();
+    var search_term = $('input').val();
+    socket.emit('updateTerm', search_term);
+  });
+  socket.on('updatedTerm', function(searchTerm) {
+    $('h1').text("Twitter Search for "+ searchTerm);
+    console.log(searchTerm);
+  });
+});
