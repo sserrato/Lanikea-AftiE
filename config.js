c878032079bbaf9586eaff9c7858bb8fea6decfd
pericles <pericles3d@gmail.com>
2015-08-21 10:16:43 -0700

    edited stylized-markers.html

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'WcaAQBXZYGfPHJ70SgkY5zw3E',
-  consumer_secret: 'zrPIX3kbXXLqUV9UzjVvWujjTRv0QlucAcCIUoPP7jAosWyPCs',
-  access_token_key: '598086193-tecLkrAQjgodteh2TqroeIwBV4TjvXoq016vCWqd',
-  access_token_secret: 'iywju2bkmAp5SS9ufYeydMlaPCOUvkkvYx5DD11vQoJRh'
-};
