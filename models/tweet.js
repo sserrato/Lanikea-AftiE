d07b818e4c2f89b186ba9eaac1b6545f66112110
Sergio Serrato <serrato@cal.berkeley.edu>
2015-08-20 18:24:42 -0700

    This is the api data for the dashboard.

--- models/tweet.js
+++ models/tweet.js
@@ -3,7 +3,13 @@ var mongoose  = require('mongoose');
 var tweetstreamSchema = new mongoose.Schema({
   queryTerm: {type: String},
   followersCount: {type: Number},
-  friendsCount: {type: Number}
+  friendsCount: {type: Number},
+  coordinates: {type: [Number]},
+  country: {type: String},
+  screenName: {type: String},
+  listedCount: {type: Number},
+  statusesCount: {type: Number},
+  favouritesCount: {type: Number}
 })
 
 
